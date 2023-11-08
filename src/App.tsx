ce1efa465228fa4fd0146e39835e5759fe0da1fa
Júlio César <juliowanke@gmail.com>
2023-11-08 14:55:35 -0300

    feat: particles-container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import styles from './App.module.scss';
+import ParticlesContainer from './Components/ParticlesContainer/ParticlesContainer';
+
 const App = () => {
-  return <div ></div>;
+  return (
+    <div className={styles.mainContainer}>
+      <ParticlesContainer />
+    </div>
+  );
 };
 
 export default App;
