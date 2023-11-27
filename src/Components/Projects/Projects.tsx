d8a7ba8084c19786f8971f3b20629623a1110c75
Júlio César <juliowanke@gmail.com>
2023-11-27 17:44:17 -0300

    Refactor: Update carousel  and add hover effects

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -2,18 +2,35 @@ import React from 'react';
 import styles from './Projects.module.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { MdOutlineArrowLeft } from 'react-icons/md';
 import { MdOutlineArrowRight } from 'react-icons/md';
 
-import {
-  Autoplay,
-  EffectCoverflow,
-  Pagination,
-  Navigation,
-} from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
+
+interface Project {
+  srcImage: string;
+  altImage: string;
+  urlGitHub: string;
+  urlProject: string;
+}
+
+const projects: Project[] = [
+  {
+    srcImage: '/AnimaisFantásticos.png',
+    altImage: 'Projeto AnimaisFantásticos',
+    urlGitHub: 'https://github.com/JuliocesarWpedro/animais-fantasticos',
+    urlProject:
+      'https://animais-fantasticos-git-master-julio-cesar-w-pedros-projects.vercel.app/',
+  },
+  {
+    srcImage: '/Pokedex.png',
+    altImage: 'Projeto Pokedex',
+    urlGitHub: 'https://github.com/JuliocesarWpedro/pokedexNext',
+    urlProject: 'https://pokedex-next-julio.vercel.app/',
+  },
+];
 
 const Projects = () => {
   return (
@@ -24,63 +41,41 @@ const Projects = () => {
       </div>
       <div>
         <Swiper
-          effect={'coverflow'}
+          slidesPerView={'auto'}
+          spaceBetween={30}
+          scrollbar={{
+            hide: true,
+          }}
+          loop={true}
           grabCursor={true}
           centeredSlides={true}
-          slidesPerView={'auto'}
-          loop={true}
-          autoplay={{ delay: 5000 }}
-          coverflowEffect={{
-            rotate: 15,
-            stretch: 0,
-            depth: 300,
-            modifier: 1,
-            slideShadows: true,
-          }}
           pagination={{ el: '.swiper-pagination', clickable: true }}
           navigation={{
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
           }}
-          modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
-          className="swiper_container"
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
         >
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>{' '}
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/AnimaisFantásticos.png" alt="" />
-          </SwiperSlide>
+          {projects.map((project) => {
+            return (
+              <SwiperSlide className={styles.swiperSlideCard}>
+                <img src={project.srcImage} alt={project.altImage} />
+                <div className={styles.projectsSlideLinks}>
+                  <a
+                    rel="noreferrer noopener"
+                    target="__blank"
+                    href={project.urlProject}
+                  >
+                    VIEW PROJECT
+                  </a>
+                  <a target="__blank" href={project.urlGitHub}>
+                    VIEW CODE
+                  </a>
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
         <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
