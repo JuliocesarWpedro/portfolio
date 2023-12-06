98763db39bd2803c2ff2b8aadb9fb51a57ec2ef4
Julio Cesar Wanderosfky Pedro <juliowanke@gmail.com>
2023-12-06 19:20:22 -0300

    refactor: footer moved to main and header to App

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/global.scss';
 import './i18n/i18n.tsx';
-import Header from './Components/Header/Header.tsx';
 import { LanguageProvider } from './context/LanguageContext.tsx';
 import ParticlesContainer from './Components/ParticlesContainer/ParticlesContainer.tsx';
+import Footer from './Components/Footer/Footer.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LanguageProvider>
       <ParticlesContainer />
-      <Header />
       <App />
+      <Footer />
     </LanguageProvider>
   </React.StrictMode>,
 );
